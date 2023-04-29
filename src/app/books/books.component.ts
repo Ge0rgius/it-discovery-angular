07c2cae14e4ca1f1792f65b6b657610a77bd5f43
Sergiy <sergey.morenets@gmail.com>
2023-04-29 16:44:42 +0300

    Task 7

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,34 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Book} from "../book";
+import {BookService} from "../book.service";
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css']
 })
-export class BooksComponent {
+export class BooksComponent implements OnInit {
 
-  books: Book[];
+  books?: Book[];
 
-  constructor() {
-    this.books = [{
-      title: 'JavaScript: The Good Parts',
-      author: 'Douglas Crockford', pages: 172, year: 2008,
-      description:
-        ' This authoritative book scrapes away these bad features to reveal a subset of JavaScript that\'s more reliable, readable, and maintainable'
-    }, {
-      title: 'Mastering TypeScript',
-      author: 'Nathan Rozentals',
-      description: 'Build enterprise-ready, industrial strength web applications using TypeScript and leading JavaScript frameworks',
-      year: 2015,
-      pages: 364
-    }, {
-      title: 'Switching to Angular 2',
-      author: 'Minko Gechev',
-      description: 'Start using TypeScript to supercharge your Angular 2 applications',
-      year: 2016,
-      pages: 254
-    }];
+  constructor(private bookService: BookService) {
+
+  }
+
+  ngOnInit(): void {
+    this.books = this.bookService.getBooks();
   }
 
   isJavaScript(book: Book): boolean | undefined {
