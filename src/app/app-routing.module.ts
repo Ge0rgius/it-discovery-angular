eb360b2b3e8daea0baa1c18f3879f86e03905716
Sergiy <sergey.morenets@gmail.com>
2023-04-30 17:23:26 +0300

    Task 19

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {BooksComponent} from "./books/books.component";
+import {NewsComponent} from "./news/news/news.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'books', component: BooksComponent
+},
+  {path: '', redirectTo: 'books', pathMatch: 'full'},
+  {path: 'news', component: NewsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
