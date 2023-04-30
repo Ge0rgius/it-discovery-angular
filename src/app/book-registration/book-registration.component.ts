be3df17e59a8bafd0ab5ae417de1d44aac269c64
Sergiy <sergey.morenets@gmail.com>
2023-04-30 10:21:33 +0300

    Task 10. DomSanitizer

--- src/app/book-registration/book-registration.component.ts
+++ src/app/book-registration/book-registration.component.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, SecurityContext} from '@angular/core';
 import {BookService} from "../book.service";
 import {Book} from "../book";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-book-registration',
@@ -12,7 +13,8 @@ export class BookRegistrationComponent {
 
   bookForm: FormGroup;
 
-  constructor(private bookService: BookService, formBuilder: FormBuilder) {
+  constructor(private bookService: BookService, formBuilder: FormBuilder,
+              private sanitizer: DomSanitizer) {
     this.bookForm = formBuilder.group({
       title: formBuilder.control('', [Validators.required,
         Validators.minLength(4), Validators.maxLength(50)]),
@@ -23,6 +25,7 @@ export class BookRegistrationComponent {
 
   register(): void {
     const book = this.bookForm.value as Book;
+    book.title = this.sanitizer.sanitize(SecurityContext.SCRIPT, book.title || '') as string;
     this.bookService.save(book);
   }
 
